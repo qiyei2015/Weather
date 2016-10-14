9eee23034be1da5691d27f730fb50d7053d4451c
qiyei2015 <1273482124@qq.com>
2016-10-14 23:08:52 +0800

    重构v0.2

---
+++ app/src/main/java/com/qiyei/weather/WeatherApplication.java
@@ -0,0 +1,16 @@
+package com.qiyei.weather;
+
+import android.app.Application;
+
+/**
+ * Created by qiyei2015 on 2016/10/14.
+ * 1273482124@qq.com
+ */
+
+public class WeatherApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
---
+++ app/src/main/java/com/qiyei/weather/activity/BaseActivity.java
@@ -0,0 +1,21 @@
+package com.qiyei.weather.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by qiyei2015 on 2016/10/14.
+ * 1273482124@qq.com
+ */
+
+public class BaseActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+
+}
